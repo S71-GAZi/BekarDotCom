b43616a817332815b6e6f8d445de44e72a0cc145
“S71-GAZi <sorifullahpavelgazi@gmail.com>
2023-01-29 15:52:07 +0600

    first setup

---
+++ Bekar.Com/Bekar.Com/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Bekar.Com.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
